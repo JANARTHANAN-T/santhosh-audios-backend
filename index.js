51bfd8fbc7333af1a498173461643969ba7fdc01
manibharthi <manibharthi@gmail.com>
2023-02-15 15:34:32 +0530

    setup/init/server

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const path =require('path')
+const app = express();
+app.listen(5000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server Started Successfully.");
+  }
+});
+mongoose
+  .connect("mongodb+srv://owndb:VInxk9KEBPe3RMcU@cluster0.ncpvelk.mongodb.net/Santhosh-audios", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use(cors());
+app.use(cookieParser());
+app.get('/', (req, res) => {
+  res.send("GET Request Called")
+})
+app.use(express.json());
+app.use("/api", authRoutes);
